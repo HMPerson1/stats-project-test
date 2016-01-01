7c3e1b146426459d3822d8b0c056a4e5b1797712
HMPerson1 <hmperson1+github@gmail.com>
2016-01-01 14:28:26 -0500

    Improve performance
    In `varianceScore`, we can delay the usage of `Rational`s until after
    summing the list.
    
    Also turn on -fno-warn-type-defaults because readability

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
 module Lib
     ( expectedScore
     , varianceScore
@@ -8,7 +9,9 @@ import Params
 import Language.Haskell.TH
 import Control.Monad ( replicateM )
 import GHC.Base      ( build )
-import Data.Ratio    ( (%) )
+import Data.Ratio    ( (%), numerator, denominator )
+
+default (Int)
 
 allScores :: [Int]
 allScores = map score (permute [1..die])
@@ -17,23 +20,25 @@ permute :: [a] -> [[a]]
 permute xs = $(do
     names <- replicateM gameLen (newName "x")
     return (CompE
-           ((map (\x -> BindS (VarP x) (VarE (mkName "xs"))) names)
+           (map (\x -> BindS (VarP x) (VarE (mkName "xs"))) names
            ++ [NoBindS (ListE (map VarE names))])))
 
 allScoresLen :: Integer
 allScoresLen = fromIntegral (die ^ gameLen)
 
 expectedScore :: Rational
-expectedScore = ((sum . map fromIntegral $ allScores) % allScoresLen)
+expectedScore = (sum . map fromIntegral $ allScores) % allScoresLen
 
 stddevScore :: Double
 stddevScore = sqrt (fromRational varianceScore)
 
 varianceScore :: Rational
-varianceScore = (sum . map (\n -> ((fromIntegral n)-expectedScore) ^ (2::Int)) $ allScores) / fromInteger allScoresLen
+varianceScore = (sum . map (\n -> (b*fromIntegral n - a)^2) $ allScores) % (b^2 * allScoresLen)
+    where a = numerator expectedScore
+          b = denominator expectedScore
 
 score :: [Int] -> Int
-score = sum . map (\(a,b) -> (a ^ (2::Int)) * b) . runLengthEncode
+score = sum . map (\(a,b) -> (a^2) * b) . runLengthEncode
 
 -- copied from:
 -- https://wiki.haskell.org/99_questions/Solutions/10
